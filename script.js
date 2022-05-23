342a5263b912449fed66ffe49ae864fa754760ef
Valesh Gopal <valesh.dev@gmail.com>
2022-05-23 11:36:07 +0530

    Add hover board app

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+const container = document.getElementById("container");
+const colors = [
+  "purple",
+  "magenta",
+  "cyan",
+  "pink",
+  "lightgreen",
+  "lightblue",
+  "tomato",
+];
+const squares = 1596;
+
+for (let i = 0; i < squares; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", function () {
+    setColor(square);
+  });
+
+  square.addEventListener("mouseout", function () {
+    removeColor(square);
+  });
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.background = "#1d1d1d";
+  element.style.boxShadow = "0 0 5px rgba(0, 0, 0, 0.5)";
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
